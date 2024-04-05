6a4d8fc0d75a46d705ad12b4dece22a208e4830f
anteater@mac <anteater1056@gmail.com>
2024-04-05 21:31:39 +0900

    MyForm

---
+++ src/components/MyForm.jsx
@@ -0,0 +1,9 @@
+function MyForm() {
+  return (
+    <form action="https://randomuser.me/api/" method="get">
+      <button type="submit">Submit Form</button>
+    </form>
+  );
+}
+
+export default MyForm;
